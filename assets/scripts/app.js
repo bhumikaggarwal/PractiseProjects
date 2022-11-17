259748e99ab57a08f5d25b164e4879c9bb3c0d2f
BhumikaAggarwal <aggarwalbhumika99@gmail.com>
2022-11-17 11:11:47 +0530

    you can now add valid inputs to the site

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,8 @@ const startAddMovieButton = document.querySelector('header button');
 //const startAddMovieButton = document.querySelector('header').lastElementChild;
  const backdrop = document.querySelector('body').firstElementChild;
 
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+
  const togglingBackdrop = () => {
     backdrop.classList.toggle('visible');
  };
@@ -14,4 +16,12 @@ const togglingTheMovieButton = () => {   //function() {}
     addMovieModal.classList.toggle('visible');
     togglingBackdrop();
 };
+const backdropClickHandler = () => {
+    togglingTheMovieButton();
+};
+const cancelButtonClickHandler = () => {
+    togglingTheMovieButton();
+}
 startAddMovieButton.addEventListener('click',togglingTheMovieButton);
+backdrop.addEventListener('click',backdropClickHandler);
+cancelAddMovieButton.addEventListener('click',cancelButtonClickHandler);
